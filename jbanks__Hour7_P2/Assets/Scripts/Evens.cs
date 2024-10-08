8fa539a428742fe9885739e94140639f85a2e8f1
JoshuaYBanks <jbanks002@student.sdccd.edu>
2024-10-08 15:29:03 -0700

    forgot to sent p2 of 7

---
+++ jbanks__Hour7_P2/Assets/Scripts/Evens.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Evens : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int i = 22;
+        while (i <101){
+             Debug.Log(i);
+             i++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
