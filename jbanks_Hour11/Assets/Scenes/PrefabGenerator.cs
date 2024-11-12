ab32075847c694aea1e06dcac24d181cf859fadb
JoshuaYBanks <jbanks002@student.sdccd.edu>
2024-11-12 05:21:00 -0800

    Finished hour 11 (actually this time)

--- jbanks_Hour11/Assets/Scenes/PrefabGenerator.cs
+++ jbanks_Hour11/Assets/Scenes/PrefabGenerator.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class PrefabGenerator : MonoBehaviour
 {
     public GameObject prefab;
+    public float spaces = 2.0f; 
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector3 first = transform.position - new Vector3((spaces * (10 - 1)) / 2f, 0, 0);
+        for (int i = 1; i <= 10; i++) {
+             Vector3 spot = first + new Vector3(i * spaces, 0, 0);
+             Instantiate(prefab, spot, transform.rotation);
+        }
     }
 
     // Update is called once per frame
